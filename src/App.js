1230624f5734140b74b4bf53438c7cbcce04a7f1
Cody <codybarker.or@gmail.com>
2023-05-10 10:17:52 -0700

    adding comments to app re project requirements and features

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ import NavBar from './NavBar'
 import './App.css';
 
 function App() {
+  /**
+   * Deliverables
+   * [x]Use Active Record
+   * [x] At least two models with a one-to-many relationship
+   * [] Create and use at least Create and Read actions in Sinatra for both models
+   * [] Full CRUD for one model
+   * [] Update action should use a pre-filled form with the existing values for the object.
+   * [] On submission of the update form, the object should update/re-render
+   * [] Build a React front end that interacts with the API to perform CRUD actions
+   * [] Proper front end state management. Do NOT rely on GET requests to update state.
+   * [] RESTful routes
+   * [] Use JSON responses to get new data. Don't rely on filtering state or separate fetch requests. 
+   * 
+   * Features
+   * - The API has a farms table and an associated beds table. A farm has many beds, and a bed belongs to a farm.
+   * - A user can create new farms and new beds. (CREATE)
+   * - A user can view a table of all farms and all beds. (READ)
+   * - A user can update a farm. (UPDATE)
+   * - A user can delete farms and beds. (DELETE)
+   * 
+   * App
+   * 
+   * 
+   */
 
   const [allFarms, setAllFarms] = useState([])
 
