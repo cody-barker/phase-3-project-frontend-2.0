929de6be15ff673e476268d0faed2647f6e61cd3
Cody <codybarker.or@gmail.com>
2023-05-10 14:23:04 -0700

    rendering TableRows to the ViewFarms table

--- src/TableRow.js
+++ src/TableRow.js
@@ -1,8 +1,39 @@
 import React from 'react'
 
-function TableRow() {
+function TableRow({bed, farmName, allFarms, setAllFarms}) {
+    
+    //A TableRow is created by inputting the values of a single bed
+    //I need to map over an array of every bed to create each table row
+    //I can get an array of every bed by mapping over each farms beds and returning each bed
+
+    const {id, sq_ft, in_use, crop, dtm, planting_date, harvest_date} = bed
+
+    // function onDelete(){
+    //     fetch(`http://localhost:9292/beds/${id}`, {
+    //         method: "DELETE"
+    //     })
+    //     .then(r => r.json())
+    //     .then(bed => {
+    //         setAllFarms([...allFarms].filter(obj => obj.id !== id))
+    //     })
+    // }
+
+    //add onClick={onDelete}
+
     return(
-        <div></div>
+        <tr>
+            <td>{farmName}</td>
+            <td>{id}</td>
+            <td>{sq_ft}</td>
+            <td>{in_use}</td>
+            <td>{crop}</td>
+            <td>{dtm}</td>
+            <td>{planting_date}</td>
+            <td>{harvest_date}</td>
+            <td>
+                <button className="delete">Delete</button>
+            </td>
+        </tr>
     )
 }
 
--- src/ViewFarms.js
+++ src/ViewFarms.js
@@ -3,13 +3,26 @@ import TableRow from './TableRow'
 
 function ViewFarms({allFarms}) {
 
-    //tableRowComps = allFarms.map(farm => <TableRow farm={farm}/>)
+    console.log(allFarms)
+
+    const tableRowComps = []
+
+    for (let i=0; i < allFarms.length; i++) {
+        tableRowComps.push(allFarms[i].beds.map(bed => <TableRow key={bed.id} bed={bed} farmName={allFarms[i].name}/>))
+    }
+
+    console.log(tableRowComps)
+
+    //allFarms.map(farm => <TableRow farm={farm} key={farm.beds.index}/>)
+    // const testBeds = allFarms[0].map(bed => <TableRow bed={bed}/>)
+    // console.log(testBeds)
 
     return(
         <div>
             <table>
                 <thead>
                     <tr>
+                    <th>Farm Name</th>
                     <th>Bed ID</th>
                     <th>Sq Ft</th>
                     <th>In Use</th>
@@ -21,7 +34,7 @@ function ViewFarms({allFarms}) {
                     </tr>
                 </thead>
                 <tbody>
-                
+                    {tableRowComps}
                 </tbody>
             </table>
         </div>
