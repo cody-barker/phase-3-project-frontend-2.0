892ed5797e34eeb1a14a6d4b83f3a4b337a6538a
Cody <codybarker.or@gmail.com>
2023-05-11 10:11:38 -0700

    adding selector and delete button for remove a farm

--- src/AddRemoveFarm.js
+++ src/AddRemoveFarm.js
@@ -4,8 +4,8 @@ function AddRemoveFarm({allFarms, setAllFarms}) {
     /**
      * Deliverables
      * 
-     * [] Have a controlled form that allows the user to add a farm
-     * [] Have a controlled form that allows the user to delete a farm
+     * [x] Have a controlled form that allows the user to add a farm
+     * [] Allows the user to delete a farm
      */
 
     const [farmName, setFarmName] = useState("")
@@ -23,14 +23,15 @@ function AddRemoveFarm({allFarms, setAllFarms}) {
     function onFarmStateChange(e) {
         setFarmState(e.target.value)
     }
-    
-    console.log(farmName)
 
     const newFarm = {
         name: farmName,
         city: farmCity,
         state: farmState,
     }
+
+    //add a condition to updating state with the new farm only if the db addition is uniq
+
 
     function onAddFarm(e) {
         e.preventDefault()
@@ -42,20 +43,33 @@ function AddRemoveFarm({allFarms, setAllFarms}) {
             body: JSON.stringify(newFarm)
         })
         .then(r => r.json())
-        .then(farm => setAllFarms([...allFarms, farm]))
+        .then(farm => {
+            setAllFarms([...allFarms, farm])
+            setFarmName("")
+            setFarmCity("")
+            setFarmState("")
+        })
     }
 
     return(
         <div>
-            <form onSubmit={onAddFarm}>
-                <label>Farm Name</label>
-                <input onChange={onFarmNameChange} value={farmName}  type="text"></input>
-                <label>City</label>
-                <input onChange={onFarmCityChange} value={farmCity} type="text"></input>
-                <label>State</label>
-                <input onChange={onFarmStateChange} value={farmState} type="text"></input>
-                <button className="update-btn" type="submit">Submit</button>
-            </form>
+            <div className="add-farm-container">
+                <h2>Add a Farm</h2>
+                <form onSubmit={onAddFarm}>
+                    <label>Farm Name</label>
+                    <input onChange={onFarmNameChange} value={farmName}  type="text"></input>
+                    <label>City</label>
+                    <input onChange={onFarmCityChange} value={farmCity} type="text"></input>
+                    <label>State</label>
+                    <input onChange={onFarmStateChange} value={farmState} type="text"></input>
+                    <button className="update-btn" type="submit">Submit</button>
+                </form>
+            </div>
+            <div className="remove-farm-container">
+                <h2>Remove a Farm</h2>
+                <select>{allFarms.map(farm => <option farm={farm} key={farm.id}>{farm.name}</option>)}</select>
+                <button class="delete">Delete</button>
+            </div>
         </div>
     )
 }
